7c7768cc3b18556c96ca65fe2b413c23db7bbd89
Kalavalapalli Mohan <kalavalapallimohan9@gmail.com>
2026-07-17 13:34:36 +0530

    Completed Phase 5 Day 8 Admin Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -9,31 +11,50 @@ import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-function App() {
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function PortfolioWebsite() {
   return (
     <>
       <Header />
 
       <main>
         <Hero />
-
         <About />
-
         <Skills />
-
         <Resume />
-
         <Certificates />
-
         <Portfolio />
-
         <Services />
-
         <Contact />
       </main>
 
       <Footer />
     </>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      {/* Public Portfolio */}
+      <Route path="/" element={<PortfolioWebsite />} />
+
+      {/* Login */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Protected Admin */}
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
