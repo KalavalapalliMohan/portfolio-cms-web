b6e85932b4095e46b9b16b0f959a8e7720adce0c
Kalavalapalli Mohan <kalavalapallimohan9@gmail.com>
2026-07-29 14:26:28 +0530

    Add AOS animations and improve portfolio UI interactions

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+function Root() {
+
+  useEffect(() => {
+
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+      offset: 100,
+    });
+
+  }, []);
+
+
+  return (
     <AuthProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </AuthProvider>
+  );
+
+}
+
+
+ReactDOM.createRoot(document.getElementById("root"))
+.render(
+  <Root />
 );
